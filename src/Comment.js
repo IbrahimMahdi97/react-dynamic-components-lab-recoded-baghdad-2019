7c4d7b6f361f149db941fe41784ee280610f1a89
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-16 09:54:59 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,3 +1,13 @@
 //your code here
 import React, {Component} from react;
 import BlogPost from './BlogPost.js'
+
+export default class Comment extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.commentText}
+      </div>
+    );
+  }
+}
